b785da2ff9a85f0a89e58556730dc8e6050ccdba
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 15:47:04 +0300

    size method

---
+++ project 1/size.js
@@ -0,0 +1,17 @@
+const map1 = new Map();
+
+map1.set('a', 1);
+map1.set('b', 2);
+map1.set('c', 3);
+
+
+
+
+
+
+console.log(map1.size);
+// expected output: 3
+
+
+console.log(map1.size);
+// expected output:
